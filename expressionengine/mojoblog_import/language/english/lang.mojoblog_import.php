66c9d6d25ed7ed1f9af3159c489efa76cddcae86
Jamie Rumbelow <jamie@jamierumbelow.net>
2010-07-26 13:23:31 +0100

    Skeleton and language files for MojoBlog Importer

---
+++ expressionengine/mojoblog_import/language/english/lang.mojoblog_import.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * MojoBlog
+ *
+ * A small, quick, and painfully simple 
+ * blogging system for MojoMotor 
+ *
+ * @package mojoblog
+ * @author Jamie Rumbelow <http://jamierumbelow.net>
+ * @copyright (c)2010 Jamie Rumbelow
+ */
+
+$lang = array(
+	// Module information
+	"mojoblog_import_module_name" => "MojoBlog Importer",
+	"mojoblog_import_module_description" => "Import all your blog data from MojoBlog into ExpressionEngine",
+	
+	// Done!
+	"" => ""
+);
