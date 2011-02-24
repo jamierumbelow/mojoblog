bebe557713756638b4d4f45a417492351817b694
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-02-24 11:02:53 +0000

    Made a start on a basic entry listing

--- third_party/blog/views/index.php
+++ third_party/blog/views/index.php
@@ -1,4 +1,27 @@
-<table>
-	<tr><th>Header</th></tr>
-	<tr><td>Data</td></tr>
+<table class="mojo_table" id="mojoblog_entries">
+	<thead>
+		<tr>
+			<th>Title</th>
+			<th>Publish Date</th>
+			<th>Status</th>
+			<th></th>
+			<th></th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php foreach($entries as $entry): ?>
+			<tr class="<?=alternator('even','odd')?>">
+				<td><?=$entry->title?></td>
+				<td><?=date('Y/m/d', strtotime($entry->date))?></td>
+				<td></td>
+				<td><?=anchor('admin/addons/blog/edit/' . $entry->id, 'Edit', 'class="mojo_sub_page" title="Edit Entry"')?></td>
+				<td><?=anchor('admin/addons/blog/delete_confirm/' . $entry->id, 'Delete', 'class="mojo_sub_page" title="Delete Entry"')?></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
 </table>
+
+<div class="shrinkwrap">
+	<p><?=anchor('admin/addons/blog/create', 'Add Post', 'class="mojo_sub_page shrinkwrap_submit" title="Add Post"')?></p>
+</div>
